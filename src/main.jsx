990ca9c838673bae198d400817ba506d26bf515c
Mohd Sadab <mohdsadab147@gmail.com>
2026-07-05 00:00:24 -0700

    Add main.jsx and latest changes

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      {/* Tumhare Routes */}
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
